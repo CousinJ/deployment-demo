7fcbdd73ab67bda45fbe34b05af71431086f15c2
Jason Davis <Jasonodavis323@gmail.com>
2022-07-20 12:34:01 -0600

    style added

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const app = express()
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
+
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../style.css'))
+})
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {console.log(`server is running on port ${port}`)})
